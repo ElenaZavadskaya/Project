1ab31de72673702031ddaa996e314f76898714ff
Elena <ellllllllllllllllllen@mail.ru>
2021-10-04 17:41:52 +0300

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,21 @@
+import aquality.selenium.core.logging.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Main {
+    @Test
+    public void test(){
+        Logger.getInstance().info("Executing test 1");
+        String actual = "str";
+        String expected = "str";
+        Assert.assertEquals(actual, expected, "Messages are not equal");
+    }
+
+    @Test
+    public void test2(){
+        Logger.getInstance().info("Executing test 2");
+        boolean actual = true;
+        boolean expected = true;
+        Assert.assertEquals(actual, expected, "Values are not equal");
+    }
+}
